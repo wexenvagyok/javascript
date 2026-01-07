ac9581b252228baa083b44c2c61faa633ca63fc4
unknown <farkas.balazs.22@ady-nagyatad.lan>
2026-01-07 09:34:07 +0100

    muveletek

--- 12b1/phpapi/miniapi.php
+++ 12b1/phpapi/miniapi.php
@@ -1,6 +1,22 @@
 <?php
 $a = (int)$_GET["a"] ?? 0;
 $b = (int)$_GET["b"] ?? 0;
+$muvelet = $_GET["muvelet"] ?? "";
 
-echo $a+$b;
-?>
+switch ($muvelet) {
+    case 'osszead':
+        echo $a + $b;
+        break;
+
+    case 'kivon':
+        echo $a - $b;
+        break;
+
+    case 'szoroz':
+        echo $a * $b;
+        break;
+
+    default:
+        echo "Hiba!";
+        break;
+}
